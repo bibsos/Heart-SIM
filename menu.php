8ffd37c3d4b07c614db1fe2437463e73ca6f021a
Lenovo <imo.goncalves@campus.fct.unl.pt>
2022-05-11 16:33:25 +0100

    menu

--- menu.php
+++ menu.php
@@ -3,7 +3,7 @@
 ?>
     <table width="100%" border="1">
     <tr>
-        <th> Início </th>
+        <th> Início ff</th>
         <th> <a href="showlogin.php"> Iniciar Sessão </a></th>
     </tr>
 
