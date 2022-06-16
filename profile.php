abe65cee01694f69107ac6526a0b9a16d80f9299
Lenovo <imo.goncalves@campus.fct.unl.pt>
2022-06-16 18:53:08 +0100

    atualizacao

--- profile.php
+++ profile.php
@@ -2,7 +2,7 @@
     $connect = mysqli_connect('localhost', 'root', '','heartsim')
     or die('Error connecting to the server: ' . mysqli_error($connect));
     $ID = $_SESSION['ID'];
-    $query = "SELECT `Nome`, `Morada`, `Contacto`, `Fotografia`, `username` FROM `utilizador` AS u WHERE u.ID = $ID";
+    $query = "SELECT `Nome`, `Morada`, `Contacto`, `Fotografia`, `username` FROM `users` AS u WHERE u.ID = $ID";
     $result = mysqli_query($connect, $query) or die('The query failed'.mysqli_error($connect));
     $row = mysqli_fetch_array($result);
 
